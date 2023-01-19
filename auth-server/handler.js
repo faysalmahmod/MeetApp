1cf5860c54c2ee38edc65134aec94bbae292abc0
Faisal mahmood <faysalmahmod7@gmail.com>
2023-01-19 13:09:22 +0100

    Update handler.js

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -19,7 +19,7 @@ const credentials = {
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  redirect_uris: ["https://faysalmahmod.github.io/MeetApp/"],
+  redirect_uris: ["https://faysalmahmod.github.io/MeetApp/","https://faysalmahmod.github.io/MeetApp"],
   javascript_origins: ["https://faysalmahmod.github.io", "http://localhost:3000"],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
